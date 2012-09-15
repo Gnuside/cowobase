52fc5528e60cbb2f23e6a32bdffca720130fb7d3
Glenn Y. Rolland <glenux@glenux.net>
2012-09-15 16:45:41 +0200

    Initial import.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cowobase::Application.config.secret_token = 'b51ffd1b762262346a35b5e780b30089eeaf0844bb6809d83f40a4bfe6cd5f1fb60c1b0298a698dd20df24b822dc38ebd47f3ceea301c3f687b3a908dac4def3'
